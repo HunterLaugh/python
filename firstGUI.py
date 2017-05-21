7ad493ffdab1533b6842a1ee700f0c407423bb95
home <493418400@qq.com>
2017-05-21 21:16:38 +0800

    add gui

---
+++ firstGUI.py
@@ -0,0 +1,24 @@
+# coding:utf-8
+
+from tkinter import *
+
+class Application(Frame):
+	def _init_(self,master=None):
+		Frame.__init__(self,master)
+		self.pack()
+		self.createWidgets()
+		
+	def createWidgets(self):
+		self.helloLabel=Label(self,text='Hello, world!')
+		self.helloLabel.pack()
+		self.quitButton=Button(self, text='Quit',command=self.quit)
+		self.quitButton.pack()
+
+#　实例化		
+app=Application()
+
+# 设置窗口标题
+app.master.title('Hello World')
+ojniouihuipi-063/07
+app.mainloop()
+.+7p[;]=
