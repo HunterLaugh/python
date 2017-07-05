d9a5facad17bc29fda99517c8f08d02a7bb6b926
gionee <493418400@qq.com>
2017-07-05 16:41:25 +0800

    some

---
+++ stuff/ex33.py
@@ -0,0 +1,35 @@
+i=0
+numbers=[]
+
+while i<6:
+	print("At the top i is %d" % i)
+	numbers.append(i)
+	
+	i=i+1
+	print("Numbers now: ", numbers)
+	print("At the bottom i is %d" % i)
+	
+print("The numbers: ")
+
+for num in numbers:
+	print(num)
+	
+
+# test more
+def  func(n):
+	li=[]
+	i=0
+	while i<n:
+		li.append(i)
+		i=i+1
+	
+	return li
+	
+print("func(10)",func(10))
+
+# test more
+f_r=[]
+for x in range(10):
+	f_r.append(x)
+	
+print("f_r ",f_r)
